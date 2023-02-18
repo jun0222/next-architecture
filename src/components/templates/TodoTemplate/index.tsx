094a78fbb574fc0517bfc8a9ac02d06b7bf96d4d
JunHatanaka <nextseason6@gmail.com>
2023-02-19 01:14:47 +0900

    Feature: todo一覧にダミーデータ追加

--- src/components/templates/TodoTemplate/index.tsx
+++ src/components/templates/TodoTemplate/index.tsx
@@ -1,14 +1,36 @@
+import Link from "next/link";
+import { useState } from "react";
 import styles from "./styles.module.css";
 
 export const TodoTemplate = () => {
+  const dummyTodos = [
+    { id: 1, title: "1つ目", content: "1つ目の本文" },
+    { id: 2, title: "2つ目", content: "2つ目の本文" },
+    { id: 3, title: "3つ目", content: "3つ目の本文" },
+  ];
+
+  // FIXME: ContextAPIで管理する
+  const [todos] =
+    useState<{ id: number; title: string; content: string }[]>(dummyTodos);
+
   return (
-    <div className={styles.container}>
-      <div className={styles.area}>
-        <input type="text" />
+    <>
+      <div className={styles.container}>
+        <h2>Todo一覧</h2>
+        <ul className={styles.area}>
+          {todos.map((todo) => {
+            return (
+              <div key={todo.id}>
+                {`${todo.title}：${todo.content}`}
+                {/* FIXME: constantsのNavigationと整合性をとる */}
+                <Link className={styles.link} href={`todos/${todo.id}`}>
+                  詳細
+                </Link>
+              </div>
+            );
+          })}
+        </ul>
       </div>
-      <ul className={styles.area}>
-        <li>タスク</li>
-      </ul>
-    </div>
+    </>
   );
 };
